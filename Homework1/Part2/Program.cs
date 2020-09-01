46b406c32bdc8dd5677bd2f947577751ef37f6a7
westleyc30 <westleyc30@gmail.com>
2020-08-31 18:00:58 -0700

    pre program comment

--- Homework1/Part2/Program.cs
+++ Homework1/Part2/Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/// Chapter No. 1		Exercise No. 1 part 2
+/// File Name: Part2
+/// @author: Westley Cho
+/// Date:  August 24, 2020
+///
+/// Problem Statement: Write a program that displays a simple picture of your choice.
+/// It could be a car, a house, a flower, a tree, a square, a 3-D box, a star or other shapes.
+///
+/// Hint: Write a sequence of WriteLine statements that display lines of asterisks or other characters and blanks.
+/// PrintScreen your results.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Use writeline method to draw a shape on the screeen
+/// 
+
+using System;
 
 namespace Part2
 {
@@ -6,9 +22,12 @@ namespace Part2
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("###");
-      Console.WriteLine("###");
-      Console.WriteLine("###");
+      Console.WriteLine("         #          ");
+      Console.WriteLine("        # #         ");
+      Console.WriteLine("       #   #        ");
+      Console.WriteLine("      #######       ");
+      
+        
     }
   }
 }
