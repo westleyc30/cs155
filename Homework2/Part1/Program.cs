735f7957e7e4dccf52988ba90659baa81f235add
westleyc30 <westleyc30@gmail.com>
2020-09-14 17:42:15 -0700

    homework 2

---
+++ Homework2/Part1/Program.cs
@@ -0,0 +1,37 @@
+﻿/// Homework2
+/// File Name: Part1 
+/// @author: Westley Cho
+/// Date:  September 14, 2020
+///
+/// Problem Statement: Write a program that start with the string variable
+/// first set to your first name and the string variable last set to your
+/// last name. Both names should be all lowercase. Your program should then
+/// create a new string that contains your full name in pig latin with the
+/// first letter capitalized for the first and last name.  Use only the pig
+/// latin rule of moving the first letter to the end of the word and adding
+/// “ay”. Output the pig latin name to the screen. Use Substring and
+/// ToUpper methods to construct the new name.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Prompt user for first name
+/// 2) Prompt user for last name
+/// 3) Convert name to pig latin
+/// 4) Print name in pig latin to screen
+/// 
+
+using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Today we're going to pig latinify your name");
+            Console.WriteLine("What's your first name? ");
+            Console.WriteLine("What's your last name? ");
+            Console.WriteLine("In pig latin your name would be ");
+        }
+    }
+}
---
+++ Homework2/Part2/Program.cs
@@ -0,0 +1,33 @@
+﻿/// Homework 2
+/// File Name: Part2
+/// @author: Westley Cho
+/// Date:  September 14, 2020
+///
+/// Problem Statement: Write a program that converts degrees Fahrenheit to Celsius
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for a temperature in F
+/// 3) Using equation calculate the temperature in C
+/// 4) Print to screen
+/// 
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter a temperature in Farenheit");
+            string temperatureAsString = Console.ReadLine();
+            double fahrenheit = Convert.ToDouble(temperatureAsString);
+            double toCelcius = (5 * (fahrenheit - 32) / 9);
+            double celciusRounded = Math.Round(toCelcius, 1);
+            Console.WriteLine(temperatureAsString + "F converts to: " + celciusRounded + "C");
+        }
+    }
+}
