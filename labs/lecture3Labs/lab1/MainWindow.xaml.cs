5de2d9ade0ac57e7d82099f1d6f7a04a2626c891
westleyc30 <westleyc30@gmail.com>
2020-09-20 18:36:58 -0700

    lab 3 part 1

---
+++ labs/lecture3Labs/lab1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String pigLatinFirstName; // to output
+            String pigLatinLastName; // to output
+            String firstName = firstNameBox.Text;
+            String lastName = lastNameBox.Text;
+            String fullName;
+
+            pigLatinFirstName = capitalizeWord(toPigLatin(firstName));
+            pigLatinLastName = capitalizeWord(toPigLatin(lastName));
+
+            fullName = pigLatinFirstName + " " + pigLatinLastName;
+
+
+
+            MessageBox.Show(fullName);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void lastNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        static String capitalizeWord(String word)
+        {
+            String capitalizedWord;
+            capitalizedWord = word.Substring(0, 1).ToUpper() + word.Substring(1, word.Length - 1);
+            return capitalizedWord;
+        }
+        static String toPigLatin(String word)
+        {
+            String pigLatinizedWord;
+            pigLatinizedWord = word.Substring(1, word.Length - 1) + word.Substring(0, 1).ToLower() + "ay";
+            return pigLatinizedWord;
+        }
+    }
+}
