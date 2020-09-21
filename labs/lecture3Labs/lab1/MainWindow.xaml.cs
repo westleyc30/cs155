3dbbd9a4de8339ddcbc765f424680252ea4821ac
westleyc30 <westleyc30@gmail.com>
2020-09-21 13:57:33 -0700

    week 3 lab part 2

--- labs/lecture3Labs/lab1/MainWindow.xaml.cs
+++ labs/lecture3Labs/lab1/MainWindow.xaml.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿
+/// lab week 3
+/// File Name: Part1 
+/// @author: Westley Cho
+/// Date:  September 20, 2020
+///
+/// Problem Statement: Write a program that start with the string variable
+/// first set to your first name and the string variable last set to your
+/// last name. Both names should be all lowercase. Your program should then
+/// create a new string that contains your full name in pig latin with the
+/// first letter capitalized for the first and last name.  Use only the pig
+/// latin rule of moving the first letter to the end of the word and adding
+/// “ay”. Output the pig latin name to the screen. Use Substring and
+/// ToUpper methods to construct the new name.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Prompt user for first name
+/// 2) Prompt user for last name
+/// 3) Convert name to pig latin
+/// 4) Print name in pig latin to screen
+/// 
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- labs/lecture3Labs/lab2/Program.cs
+++ labs/lecture3Labs/lab2/Program.cs
@@ -6,7 +6,43 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Boolean  pluggedIn, freshBulb, lightOn;
+            String lightStatus, plugStatus, bulbStatus;
+
+            pluggedIn = true;
+            freshBulb = false;
+
+            if (pluggedIn == true && freshBulb == true)
+            {
+                lightOn = true;
+                lightStatus = "on";
+            } else
+            {
+                lightStatus = "off";
+            }
+
+            if (pluggedIn)
+            {
+                plugStatus = "plugged in";
+            } else
+            {
+                plugStatus = "disconnected, please plug in";
+            }
+
+            if (freshBulb)
+            {
+                bulbStatus = "Light bulb active";
+            } else
+            {
+                bulbStatus = "please replace bulb";
+
+            }
+            
+            Console.WriteLine("\n=================================================");
+            Console.WriteLine($"\n\tLight Status: {lightStatus}");
+            Console.WriteLine("\n=================================================\n");
+            Console.WriteLine($"\tPlug Status: {plugStatus}");
+            Console.WriteLine($"\tBulb Status: {bulbStatus}");
         }
     }
 }
