a581eec1bc02b8c730d741130f6018724dc877ea
westleyc30 <westleyc30@gmail.com>
2020-09-21 23:39:10 -0700

    started part2

--- Homework3/part2/Program.cs
+++ Homework3/part2/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//// NOT COMPLETE
+
+/// Homework3
+/// File Name: Part1 
+/// @author: Westley Cho
+/// Date:  September 21, 2020
+///
+/// Problem Statement: Write a program (Console or GUI) that determines the change to be dispensed from a vending machine. 
+/// An item in the machine can cost between 25 cents and 1 dollar, 
+/// in 5-cent increments (25, 30, 35,…,95, 100), and the machine accepts only a single dollar bill to pay for the item.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Take in amount owed
+/// 2) take in payment
+/// 3) Split payment into principal and payment towards loan
+
+using System;
 
 namespace part2
 {
@@ -6,7 +23,7 @@ namespace part2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double NICKEL, DIME, QUARTER;
         }
     }
 }
