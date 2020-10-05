5c8402e59b31c0ad79038db0248455501e48e3be
westleyc30 <westleyc30@gmail.com>
2020-10-05 13:23:49 -0700

    Homework 5

---
+++ Homework5/Project1/Program.cs
@@ -0,0 +1,36 @@
+﻿/// Homework5		Exercise No. 1
+/// File Name: Project1
+/// @author: Westley Cho
+/// Date:  October 5, 2020
+/// <summary>
+/// Problem Statement: Write code to create a copy of an array.
+/// 
+/// 
+/// Overall Plan:
+/// 1) First, start by creating an initial array. (You can use whatever type of data you want.)
+/// Let's start with 10 items.
+/// 2) Declare an array variable and assign it a new(yes, that means we'll use the new keyword) array with 10 items in it.
+/// 3) Now create a second array variable.  Give it a new array with the same length as the first.
+///    Instead of using a number for this length, use the Length property to get the size of the original array.
+/// 4) Use a loop to read values from the original array and place them in the new array.  Also, print out the contents
+///    of both arrays, to be sure everything copied correctly.
+/// </summary>
+using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] array1 = { "this", "That", "the other", "why", "Hello", "world", "beep", "boop", "boop", "bop" };
+            String[] arrayCopy = new string[array1.Length];
+
+            for(int i = 0; i < array1.Length; i++)
+            {
+                arrayCopy[i] = array1[i];
+                Console.WriteLine($"array1[{i}]: {array1[i]} – arrayCopy[{i}]: {arrayCopy[i]}");
+            }
+        }
+    }
+}
---
+++ Homework5/Project2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+/// Homework5		Exercise No. 2
+/// File Name: Project2
+/// @author: Westley Cho
+/// Date:  October 5, 2020
+/// <summary>
+/// Problem Statement: Reversing an Array. Let's make a program that uses methods to accomplish a task.  Let's take an array and reverse the contents of it
+/// <example>
+/// if you have 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, it would become 10, 9, 8 , 7 ,6, 5, 4, 3, 2, 1.
+/// </example>
+/// </summary>
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We shall reverse an array of numbers");
+            int[] numbers = GenerateNumbers();
+            Console.WriteLine("numbers:");
+            PrintNumbers(numbers);
+            Reverse(numbers);
+            Console.WriteLine("numbers reversed:");
+            PrintNumbers(numbers);
+        }
+        /// <summary>
+        /// Creates an array of numbers from 1 to 10
+        /// </summary>
+        /// <returns>Array</returns>
+        static int[] GenerateNumbers()
+        {
+            int[] newArray = new int[10];
+            for (int i = 0; i< newArray.Length; i++)
+            {
+                newArray[i] = i + 1;
+            }
+            return newArray;
+        }
+        static int[] Reverse(int[] numbers)
+        {
+            int[] reversedArray = new int[numbers.Length];
+            int a, b, temp;
+            for(int i = 0; i < numbers.Length / 2; i++)
+            {
+                a = numbers[i];
+                b = numbers[numbers.Length - i - 1];
+                temp = a;
+                numbers[i] = b;
+                numbers[numbers.Length - i - 1] = temp;
+            }
+            return numbers;
+        }
+        /// <summary>
+        /// prints each individual number in int[] numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        static void PrintNumbers(int[] numbers)
+        {
+            foreach (var i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
