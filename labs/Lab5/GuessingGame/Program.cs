5cefb9ef85091be7063adc83a1e4b2115b456b2e
westleyc30 <westleyc30@gmail.com>
2020-10-05 13:38:12 -0700

    lab 5

---
+++ labs/Lab5/GuessingGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            int randNum;
+            Random rand = new Random();
+            do
+            {
+                Console.WriteLine("guess a number between 1 and 100");
+                randNum = rand.Next(1, 101);
+                //test
+                Console.WriteLine($"pssst... you should guess {randNum}");
+                guess = int.Parse(Console.ReadLine());
+                
+                if(randNum == guess)
+                {
+                    Console.WriteLine("You guessed correctly! Nice!");
+                } else
+                {
+                    Console.WriteLine($"Sorry you guessed: {guess} but the number was: {randNum}");
+                }
+            } while (guess != randNum);
+            Console.WriteLine("bye now");
+        }
+    }
+}
