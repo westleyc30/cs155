00d45e4811f6453416f2c1b42eb316dc2066c532
westleyc30 <westleyc30@gmail.com>
2020-10-19 17:28:27 -0700

    console app 1 init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double windChillIndex(double windSpeed, double temperature)
+            {
+                double WCI;
+                if (windSpeed >= 0 && windSpeed <= 4)
+                {
+                    WCI = temperature;
+                } else if (windSpeed >= 45) 
+                {
+                    WCI = (1.6 * temperature) - 55;
+                } else
+                {
+                    WCI = 91.4 + (91.4 - temperature) * (0.02203 * temperature - (0.304 * Math.Sqrt(temperature)) - 0.474);
+                }
+                return WCI;
+            }
+            String checkOutfit(double windSpeed, double temperature, int humidity, int rain)
+            {
+                double WCI = windChillIndex(windSpeed, temperature);
+                if(WCI == 80 || humidity >= 90)
+                {
+                    return "Outfit 1";
+                } else if(WCI < 0 && rain < 70)
+                {
+                    return "outfit 2";
+                } else if(humidity <= 20 && WCI >= 30 && WCI <= 70)
+                {
+                    return "outfit 3";
+                } else
+                {
+                    return "outfit 4";
+                }
+            }
+            Console.WriteLine(checkOutfit(0, 45, 0, 0));
+
+        }
+    }
+}
