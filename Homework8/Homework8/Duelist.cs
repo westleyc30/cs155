2e8d3b4b2b073fae3c4ecea34ac15104a93b04eb
westleyc30 <westleyc30@gmail.com>
2020-11-02 08:56:53 -0800

    duelist class init

---
+++ Homework8/Homework8/Duelist.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework8
+{
+    
+    class Duelist
+    {
+        private String _name;
+        private int _accuracyNum;
+        private int _accuracyDenom;
+        private bool _alive = true;
+
+        public Duelist()
+        {
+            name = "Defaulty";
+            accuracyNum = 1;
+            accuracyDenom = 2;
+        }
+        public Duelist(String name, int accuracyNum, int accuracyDenom)
+        {
+            this.name = name;
+            this.accuracyNum = accuracyNum;
+            this.accuracyDenom = accuracyDenom;
+        }
+
+        // GETTERS/SETTERS
+        public String name { get { return _name; } set { _name = value; } }
+
+        public int accuracyNum
+        {
+            get { return _accuracyNum; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("accuracyNum", "Must be greater than 0");
+                }
+                _accuracyNum = value;
+            }
+        }
+
+        public int accuracyDenom
+        {
+            get { return _accuracyDenom; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("accuracyDenom", "Must be greater than 0");
+                }
+                _accuracyDenom = value;
+            }
+        }
+        public bool alive { get { return _alive; } set { _alive = value; } }
+
+        // OTHER METHODS
+
+        public bool shoot(Duelist target)
+        {
+            bool hit = false;
+            var rand = new Random();
+            int roll = rand.Next(1, accuracyDenom + 1);
+            if (roll <= accuracyNum)
+            {
+                hit = true;
+                target.alive = false;
+                Console.WriteLine($"{name} targets {target.name} rolls a {roll} and hits.");
+            } else
+            {
+                Console.WriteLine($"{name} targets {target.name} rolls a {roll} and misses.");
+            }
+            return hit;
+        }        
+
+    }
+}
--- Homework8/Homework8/Program.cs
+++ Homework8/Homework8/Program.cs
@@ -6,7 +6,12 @@ namespace Homework8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Duelist aaron = new Duelist("Aaron", 1, 3);
+            Duelist bob = new Duelist("Bob", 1, 2);
+            Duelist charlie = new Duelist("Charlie", 995, 1000);
+
+            aaron.shoot(bob);
+            Console.WriteLine(bob.alive);
         }
     }
 }
